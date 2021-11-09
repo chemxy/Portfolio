2d39f65289b865066634753b0a8be86c95654f83
Xingyun Chen <44486666+chemxy@users.noreply.github.com>
2021-11-09 15:30:30 -0800

    add header, footer and more components
    update header and footer including css.
    create more components for main content

---
+++ angular-framework/chemxy/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SummaryComponent } from './component/main-content/summary/summary.component';
+import { ResumeComponent } from './component/main-content/resume/resume.component';
+import { ProjectsComponent } from './component/main-content/projects/projects.component';
+import { ContactComponent } from './component/main-content/contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    FooterComponent,
+    SummaryComponent,
+    ResumeComponent,
+    ProjectsComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
