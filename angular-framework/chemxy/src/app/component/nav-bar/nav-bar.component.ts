0b6d91092857b680ea6bfb4701d056a8e3dcf18d
Xingyun Chen <44486666+chemxy@users.noreply.github.com>
2021-11-10 13:22:44 -0800

    remove logout functionality

--- angular-framework/chemxy/src/app/component/nav-bar/nav-bar.component.ts
+++ angular-framework/chemxy/src/app/component/nav-bar/nav-bar.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -8,7 +7,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor(private router:Router, private route: ActivatedRoute) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
