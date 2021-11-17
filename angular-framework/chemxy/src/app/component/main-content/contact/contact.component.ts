58d9c5fea7649f22afb38af1510e1a162d35e760
Xingyun Chen <44486666+chemxy@users.noreply.github.com>
2021-11-16 16:30:58 -0800

    update contact form

--- angular-framework/chemxy/src/app/component/main-content/contact/contact.component.ts
+++ angular-framework/chemxy/src/app/component/main-content/contact/contact.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class ContactComponent implements OnInit {
 
+  genders = ["male", "female", "unknown"];
+
   constructor() { }
 
   ngOnInit(): void {
