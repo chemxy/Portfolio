70bdb5254a91ef509ea8dc26b53276c28859d73b
Xingyun Chen <44486666+chemxy@users.noreply.github.com>
2022-01-23 17:15:15 -0800

    update summary page

--- angular-material/chemxy/src/app/app.module.ts
+++ angular-material/chemxy/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 
 
 @NgModule({
@@ -64,7 +65,8 @@ import { MatCardModule } from '@angular/material/card';
     MatDividerModule,
     MatListModule,
     MatExpansionModule,
-    MatCardModule
+    MatCardModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
