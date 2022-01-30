0b1253b6907b7bef7c9a1a985f3401f3ee6beb14
Xingyun Chen <44486666+chemxy@users.noreply.github.com>
2022-01-29 17:54:35 -0800

    update footer

--- angular-material/chemxy/src/app/component/footer/footer.component.ts
+++ angular-material/chemxy/src/app/component/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithubSquare, faLinkedin, faTwitterSquare, faYoutubeSquare } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+
+  faGithubSquare = faGithubSquare;
+  faLinkedin = faLinkedin;
+  faTwitterSquare = faTwitterSquare;
+  faYoutubeSquare = faYoutubeSquare;
 
   constructor() { }
 
