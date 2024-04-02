9e09f3350b3327827a4e20b8dca4e9f215d47612
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2024-04-02 07:17:05 -0700

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {HighlightsComponent} from "./components/highlights/highlights.component";
 import {SummaryComponent} from "./components/summary/summary.component";
 import {ServicesComponent} from "./components/services/services.component";
+import {SkillsComponent} from "./components/skills/skills.component";
 
 const routes: Routes = [
   {path: '', component: SummaryComponent},
   {path: 'about', component: HighlightsComponent},
-  {path: 'service', component: ServicesComponent},
+  {path: 'skills', component: SkillsComponent},
+  {path: 'services', component: ServicesComponent},
   {path: '**', redirectTo: ''},
 ];
 
