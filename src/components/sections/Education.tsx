62a7679397a71d0b5532038293e3ea5a2508972d
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2025-03-25 22:27:46 -0700

    update education

--- src/components/sections/Education.tsx
+++ src/components/sections/Education.tsx
@@ -1,77 +1,72 @@
 'use client';
 
-import {
-  Container,
-
-  Typography
-} from '@mui/material';
+import {Container, Typography} from '@mui/material';
 import {
   Timeline,
-  TimelineItem,
-  TimelineContent,
-  TimelineSeparator,
-  TimelineDot,
   TimelineConnector,
-  TimelineOppositeContent
+  TimelineContent,
+  TimelineDot,
+  TimelineItem,
+  TimelineOppositeContent,
+  TimelineSeparator
 } from '@mui/lab';
 import SchoolIcon from '@mui/icons-material/School';
 
 
 const educationData = [
-  {
-    year: '2016 - 2022',
-    degree: 'Bachelor of Software Engineering ',
-    school: 'University of Victoria',
-    description: ''
-  },
-  {
-    year: '2012 - 2015',
-    degree: 'High School',
-    school: 'China',
-    description: ''
-  }
+    {
+        year: '2016 - 2022',
+        degree: 'Bachelor of Software Engineering ',
+        school: 'University of Victoria',
+        location: 'Victoria, BC, Canada',
+        description: ''
+    },
 ];
 
 export default function Education() {
-  return (
-    <section id="education" className="py-20 bg-white ">
-      <Container maxWidth="lg">
-        <Typography 
-          variant="h2" 
-          className="text-4xl font-bold text-center mb-16 text-secondary"
-        >
-          Education
-        </Typography>
-
-        <Timeline position="left" className="animate-fadeIn">
-          {educationData.map((item, index) => (
-            <TimelineItem key={index}>
-              <TimelineOppositeContent>
-                <Typography className="text-primary font-light">
-                  {item.year}
+    return (
+        <section id="education" className="py-20 bg-white ">
+            <Container maxWidth="lg">
+                <Typography
+                    variant="h2"
+                    className="text-4xl font-bold text-center mb-16 text-secondary"
+                >
+                    Education
                 </Typography>
-              </TimelineOppositeContent>
-              <TimelineSeparator>
-                <TimelineDot className="bg-primary">
-                  <SchoolIcon />
-                </TimelineDot>
-                {index !== educationData.length - 1 && <TimelineConnector />}
-              </TimelineSeparator>
-              <TimelineContent>
-                  <Typography className="text-secondary mt-2">
-                    {item.degree}
-                  </Typography>
-                  <Typography variant="subtitle1" className="text-secondary-light">
-                    {item.school}
-                  </Typography>
-                  <Typography variant="body2" className="text-secondary-light mt-2">
-                    {item.description}
-                  </Typography>
-              </TimelineContent>
-            </TimelineItem>
-          ))}
-        </Timeline>
-      </Container>
-    </section>
-  );
+
+                <Timeline position="left" className="animate-fadeIn">
+                    {educationData.map((item, index) => (
+                        <TimelineItem key={index}>
+                            <TimelineOppositeContent>
+                                <Typography className="text-primary font-light">
+                                    {item.year}
+                                </Typography>
+                                <Typography className="text-secondary mt-2">
+                                    {item.degree}
+                                </Typography>
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineDot className="bg-primary">
+                                    <SchoolIcon/>
+                                </TimelineDot>
+                                {index !== educationData.length - 1 && <TimelineConnector/>}
+                            </TimelineSeparator>
+                            <TimelineContent>
+                                <Typography variant="subtitle1" className="text-secondary-light">
+                                    {item.school}
+                                </Typography>
+                                <Typography variant="body2" className="text-secondary-light mt-2">
+                                    {item.location}
+                                </Typography>
+                                <Typography variant="body2" className="text-secondary-light mt-2">
+                                    {item.description}
+                                </Typography>
+
+                            </TimelineContent>
+                        </TimelineItem>
+                    ))}
+                </Timeline>
+            </Container>
+        </section>
+    );
 } 
